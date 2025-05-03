1abc5202c62bea566d244b069a5f7de8d4172aee
Deepak Yadav <mahesh.kate@emagegroup.com>
2025-05-03 10:53:19 +0530

    prime num proogram added

---
+++ prime_num.py
@@ -0,0 +1,9 @@
+#prime num from 1 tp 50
+for num in range(2, 51):
+    is_prime = True
+    for i in range(2, int(num**0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(f"{num} is a Prime Number")
